1a56d8de799a7bf61515f23489461a97b2d32eea
Heryhelder <heryhelder@protonmail.com>
2023-09-20 18:30:42 -0300

    Correção pós conflito

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -13,11 +13,29 @@ download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip'
 ]
 
+async def main():
+    BASE_PATH = os.path.join(os.path.curdir, 'downloads')
 
-def main():
-    # your code here
-    pass
+    if not os.path.exists(BASE_PATH):
+        os.makedirs(BASE_PATH)
 
+    async with aiohttp.ClientSession() as session:
+        for uri in download_uris:
+            async with session.get(uri) as res:
+                if res.status == 200:
+                    file_name = uri.split('/')[3]
+
+                    with open(BASE_PATH + '/' + file_name, 'wb') as f:
+                        async for chunk in res.content.iter_chunked(1024):
+                            f.write(chunk)
+
+                    await extract_csv(BASE_PATH + '/' + file_name, BASE_PATH)
+
+async def extract_csv(file, dest):
+    if os.path.exists(file):
+        with ZipFile(file, 'r') as z:
+            z.extractall(dest)
+            os.remove(file)
 
 if __name__ == "__main__":
     asyncio.run(main())
