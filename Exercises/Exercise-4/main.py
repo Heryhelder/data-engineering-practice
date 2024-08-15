058eaee71d7288f8a4037ed94e8d9308f89f218b
Heryhelder <heryhelder@protonmail.com>
2024-08-15 10:10:16 -0300

    add: exercise 4 solution

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,10 +1,21 @@
-import boto3
-
+import glob
+import csv
+import json
+import os
+from flatten_json import flatten
 
 def main():
-    # your code here
-    pass
+    BASE_PATH = os.path.join(os.path.curdir, 'data')
+    files = [file for file in glob.iglob(BASE_PATH + '/**/*.json', recursive=True) if os.path.isfile(file)]
 
+    for file in files:
+        json_value = json.load(open(file))
+
+        with open(file + '.csv', 'w') as csv_file:
+            writer = csv.writer(csv_file)
+
+            writer.writerow(flatten(json_value).keys())
+            writer.writerow(flatten(json_value).values())
 
 if __name__ == "__main__":
     main()
